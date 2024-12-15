2102cc21cc4ee16675c01650b12bcd0d30612686
aminagr <minatub@gmail.com>
2024-12-15 20:51:02 +0100

    no static

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export',
-    images: {
-        unoptimized: true,  
-  
-      },
-      trailingSlash: true, 
+const nextConfig = { 
       eslint: {
         ignoreDuringBuilds: true,
       },
